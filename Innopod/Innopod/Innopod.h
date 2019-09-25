a4c3d2877a73b5cc967c86c222b7c2887013573a
Chema <ggc@Googlings-MacBook-Pro.local>
2019-09-25 10:49:29 +0200

    master finished

---
+++ Innopod/Innopod/Innopod.h
@@ -0,0 +1,19 @@
+//
+//  Innopod.h
+//  Innopod
+//
+//  Created by Chema on 25/09/2019.
+//  Copyright © 2019 Vueling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Innopod.
+FOUNDATION_EXPORT double InnopodVersionNumber;
+
+//! Project version string for Innopod.
+FOUNDATION_EXPORT const unsigned char InnopodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Innopod/PublicHeader.h>
+
+
